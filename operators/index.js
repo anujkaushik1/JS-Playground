f99fba33fc03b40e78250f3dd98cfea281310f9b
Anuj Kaushik <anujkaushik1512@gmail.com>
2024-10-03 22:01:41 +0530

    operators

---
+++ operators/index.js
@@ -0,0 +1,22 @@
+
+let a = {name: "hello"} || null;  // {name: "hello"} 
+
+let b = undefined || null;  // null
+
+let c = null || undefined || "haha"   // haha
+
+
+let k = undefined && "haha";   // undeifned
+
+
+let r = "hahah" && null;  // null
+
+// let r = "hahah" && "r";  // r
+
+let ki = true && {namawse: 'ha'}  //{namawse: 'ha'}
+
+let p = false && true   // false
+
+console.log(p);
+
+
