e43aad522c7d6711fc590a69c9759dd74eba280c
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2025-01-18 22:50:20 +0530

    Create main.js

---
+++ Concurrency/main.js
@@ -0,0 +1,89 @@
+const { Worker } = require("worker_threads");
+const os = require("os");
+
+const size = 99999990;
+
+let numOfPrime = 0;
+let onlineWorkerThreads = 0;
+
+let startTime = 0;
+
+function spwanThread(start, end, thread) {
+  const worker = new Worker("./my_worker.js");
+  
+
+  worker.on("message", (countOfPrimes) => {
+    numOfPrime += countOfPrimes;
+
+    onlineWorkerThreads++;
+
+    if (onlineWorkerThreads == os.cpus().length) {
+      console.log(
+        "total primes = ",
+        numOfPrime,
+        "  total time taken = ",
+        (new Date().getTime() - startTime)/1000 + "secs"
+      );
+    }
+  });
+
+  worker.on("error", (err) => {
+    console.error("Error:", err);
+  });
+
+  worker.postMessage({ start, end, thread });
+}
+
+function main() {
+  startTime = new Date().getTime();
+  const batchSize = Math.floor(size / os.cpus().length);
+  let start = 1;
+  let end = batchSize;
+
+  for (let thread = 1; thread <= os.cpus().length; ++thread) {
+    spwanThread(start, end, thread);
+
+    start = end + 1;
+    end = batchSize * (thread + 1);
+  }
+
+  if (size % os.cpus().length != 0) {
+    console.log(start, "=====", size);
+  }
+}
+
+// function main2(start, end, thread) {
+//   let startTime = new Date().getTime();
+//   let numOfPrime = 0;
+
+//   for (let primeToCheck = start; primeToCheck <= end; primeToCheck++) {
+//     if (primeToCheck < 2) {
+//       continue;
+//     }
+
+//     let isPrime = true;
+//     for (let i = 2; i <= Math.sqrt(primeToCheck); ++i) {
+//       if (primeToCheck % i == 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+
+//     if (isPrime) {
+//       numOfPrime++;
+//     }
+//   }
+//   console.log(
+//     "Main Thread Execution Completed: ",
+//     thread,
+//     "  processed values: ",
+//     end - start,
+//     "   time:   ",
+//     (new Date().getTime() - startTime) / 1000 + " secs"
+//   );
+//   console.log(numOfPrime);
+// }
+
+main();
+
+// main2(1, size, 1)
